166e0b04f58529d58e593241ea6776769442fafd
drewschmidt145 <drewschmidt145@gmail.com>
2023-07-20 11:03:19 -0700

    Updated scripts.js with array
    when active, start of main array within scripts.js, adding first 3 entries of pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
-alert('Hello World');
 
-let favoriteFood = 'Ribeye Steak';
-document.write(favoriteFood);
+pokemonList = [
+    bulbasaur,
+    ivysaur,
+    venusaur,
+];
+
+let bulbasaur = {
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['grass', 'poison']
+};
+
+let ivysaur = {
+    name: 'Ivysaur',
+    height: 1,
+    type: ['grass', 'poison']
+};
+
+let venusaur = {
+    name: 'Venusaur',
+    height: 2,
+    type: ['grass', 'poison']
+};
+
