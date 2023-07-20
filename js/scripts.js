b7dafabf89ddb335f65005ed634cfa1c6701d1d5
drewschmidt145 <drewschmidt145@gmail.com>
2023-07-20 11:07:42 -0700

    Revert "Updated scripts.js with array"
    This reverts commit 166e0b04f58529d58e593241ea6776769442fafd.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,4 @@
+alert('Hello World');
 
-pokemonList = [
-    bulbasaur,
-    ivysaur,
-    venusaur,
-];
-
-let bulbasaur = {
-    name: 'Bulbasaur',
-    height: 0.7,
-    type: ['grass', 'poison']
-};
-
-let ivysaur = {
-    name: 'Ivysaur',
-    height: 1,
-    type: ['grass', 'poison']
-};
-
-let venusaur = {
-    name: 'Venusaur',
-    height: 2,
-    type: ['grass', 'poison']
-};
-
+let favoriteFood = 'Ribeye Steak';
+document.write(favoriteFood);
