84297bd83f8fc3ea36fd09572078b12113aa420a
drewschmidt145 <drewschmidt145@gmail.com>
2023-07-20 21:43:27 -0700

    updated scripts.js with array
    when active, there should be a new array listing the beginning of the pokemon array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,48 @@
-alert('Hello World');
+pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        type: ['grass','poison']
+    },
+    {
+        name: 'Ivysaur',
+        height: 1,
+        type: ['grass','poison']
+    },
+    {
+        name: 'Venusaur',
+        height: 2,
+        type: ['grass','poison']
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        type: ['fire']
+    },
+    {
+        name: 'Charmeleon',
+        height: 1.1,
+        type: ['fire']
+    },
+    {
+        name: 'Charizard',
+        height: 1.7,
+        type: ['fire','flying']
+    },
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        type: ['water']
+    },
+    {
+        name: 'Wartortle',
+        height: 1,
+        type: ['water']
+    },
+    {
+        name: 'Blastoise',
+        height: 1.6,
+        type: ['water']
+    },
 
-let favoriteFood = 'Ribeye Steak';
-document.write(favoriteFood);
+]
