c9e7209b8a2f3efa3ba0969b6c01ab3a5c261775
drewschmidt145 <drewschmidt145@gmail.com>
2023-07-31 18:05:45 -0700

    Changes to scripts.js involving IIFE and loop
    when active, array should now be inside IIFE with new functions including an add as well as a getAll function.
    
    loop should be updated to continue working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,72 +1,85 @@
-const pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        types: ['grass','poison']
-    },
-    {
-        name: 'Ivysaur',
-        height: 1,
-        types: ['grass','poison']
-    },
-    {
-        name: 'Venusaur',
-        height: 2,
-        types: ['grass','poison']
-    },
-    {
-        name: 'Charmander',
-        height: 0.6,
-        types: ['fire']
-    },
-    {
-        name: 'Charmeleon',
-        height: 1.1,
-        types: ['fire']
-    },
-    {
-        name: 'Charizard',
-        height: 1.7,
-        types: ['fire','flying']
-    },
-    {
-        name: 'Squirtle',
-        height: 0.5,
-        types: ['water']
-    },
-    {
-        name: 'Wartortle',
-        height: 1,
-        types: ['water']
-    },
-    {
-        name: 'Blastoise',
-        height: 1.6,
-        types: ['water']
-    },
 
-];
+let pokemonRepository = (function () {
+    const pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            types: ['grass','poison']
+        },
+        {
+            name: 'Ivysaur',
+            height: 1,
+            types: ['grass','poison']
+        },
+        {
+            name: 'Venusaur',
+            height: 2,
+            types: ['grass','poison']
+        },
+        {
+            name: 'Charmander',
+            height: 0.6,
+            types: ['fire']
+        },
+        {
+            name: 'Charmeleon',
+            height: 1.1,
+            types: ['fire']
+        },
+        {
+            name: 'Charizard',
+            height: 1.7,
+            types: ['fire','flying']
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            types: ['water']
+        },
+        {
+            name: 'Wartortle',
+            height: 1,
+            types: ['water']
+        },
+        {
+            name: 'Blastoise',
+            height: 1.6,
+            types: ['water']
+        },
+    
+    ];
+  
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+  
+    function getAll() {
+      return pokemonList;
+    }
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+})();
+
 
 /*
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height >=2){
-        document.write(` <span class=pokemon-list> ${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!`);
-    }
-    else {
-        document.write(` <span class=pokemon-list> ${pokemonList[i].name} (height: ${pokemonList[i].height})`);
-    }
-
+pokemonRepository.getAll().function pokemonListFunction(item) {
+      if (item.height >=2){
+          document.write(` <span class=pokemon-list> ${item.name} (height: ${item.height}) - Wow, that's big!`);
+      }
+      else {
+          document.write(` <span class=pokemon-list> ${item.name} (height: ${item.height})`);
+      }
 };
 */
 
-pokemonList.forEach(pokemonListFunction);
-
-function pokemonListFunction(item) {
-    if (item.height >=2){
-        document.write(` <span class=pokemon-list> ${item.name} (height: ${item.height}) - Wow, that's big!`);
+pokemonRepository.getAll().forEach(function (pokemon) {
+    if (pokemon.height >=2){
+        document.write(` <span class=pokemon-list> ${pokemon.name} (height: ${pokemon.height}) - Wow, that's big!`);
     }
     else {
-        document.write(` <span class=pokemon-list> ${item.name} (height: ${item.height})`);
+        document.write(` <span class=pokemon-list> ${pokemon.name} (height: ${pokemon.height})`);
     }
-}
-
+});
